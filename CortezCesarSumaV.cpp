7d80fe98d63befad415cbe38cba955883cb2d227
fiama.hurtado <fiama.hurtado.ortiz@utelvt.edu.ecgit initgit addgit commit-m primer>
2023-05-07 16:06:42 -0500

    primer commit

---
+++ CortezCesarSumaV.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int c=0;
+	float x[5],a=0;
+	do{
+		cout<<"ingrese x["<<C+1<<"]:";cin>>x[0];
+		a=a+x[c];
+		c=c+1
+	}While (c<5);
+	c=0;
+		do{
+			cout<<x[c]<<endl;
+			c=c+1
+		}While (c<5);
+	cout<<endl<<a<<endl;
+	return (0);
---
+++ sumaFN.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+        float x,itt,l,t;
+	i=0;
+	cin>>l;
+	do{
+		cin>>x;
+		i= i+1;
+		a= a+x;
+	} while (i<p)
+	tb=a 
+		td= td+td x piub;
+	Cout<<tn<<"n";
+
+return 0;
+}
+
+20230409_120307.jpg
